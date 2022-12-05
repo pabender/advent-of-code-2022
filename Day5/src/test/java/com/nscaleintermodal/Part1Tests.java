db8c3e2b39f784aa385b0638414bebeadc250798
Paul Bender <paul.bender@acm.org>
2022-12-04 22:40:32 -0500

    add intellij template; prep for day 5.

---
+++ Day5/src/test/java/com/nscaleintermodal/Part1Tests.java
@@ -0,0 +1,23 @@
+package com.nscaleintermodal;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class Part1Tests {
+
+
+    @Test
+    public void testOverLaps(){
+        String dataFileName = this.getClass().getResource("testdata.txt").getFile();
+        Part1 testObject = new Part1(dataFileName);
+        //assertThat(testObject.overlaps()).isEqualTo(2);
+    }
+
+    @Test
+    public void part1OverLapsTest(){
+        String dataFileName = this.getClass().getResource("input.txt").getFile();
+        Part1 testObject = new Part1(dataFileName);
+        //assertThat(testObject.overlaps()).isEqualTo(599);
+    }
+}
